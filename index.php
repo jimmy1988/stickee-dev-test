d357fdeaf43d5fcf5f2dacba4f93a0af631189c8
James McHugh <james.mchugh1988@gmail.com>
2019-10-29 15:27:47 +0000

    new files for project startup

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+  include("../php/includes/top.php");
+  include("../php/includes/header.php");
+?>
+
+
+
+<?php
+  include("../php/includes/scripts.php");
+  include("../php/includes/footer.php");
+  include("../php/includes/bottom.php");
+?>
