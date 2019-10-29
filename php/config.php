5f481c1b8fe9558cc8450f37f5de62ba6ae31619
James McHugh <james.mchugh1988@gmail.com>
2019-10-29 17:24:53 +0000

    template import

---
+++ php/config.php
@@ -0,0 +1,6 @@
+<?php
+  !defined("AUTHOR") ? define("AUTHOR", "James McHugh") : "";
+  !defined("AUTHOR_CONTACT_NUMBER") ? define("AUTHOR_CONTACT_NUMBER", "07922955332") : "";
+  !defined("AUTHOR_WEBSITE") ? define("AUTHOR_WEBSITE", "") : "";
+  !defined("AUTHOR_EMAIL") ? define("AUTHOR_EMAIL", "james.mchugh1988@gmail.com") : "";
+?>
