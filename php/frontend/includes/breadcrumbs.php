3cd68378a09532d5cec25a60b9ea8e0eb0ec421c
James McHugh <james.mchugh1988@gmail.com>
2019-10-29 20:10:19 +0000

    hide breadcrumbs

--- php/frontend/includes/breadcrumbs.php
+++ php/frontend/includes/breadcrumbs.php
@@ -2,16 +2,16 @@
   <div class="container">
     <div class="row d-flex align-items-center flex-wrap">
       <div class="col-md-7">
-        <h1 class="h2">White Blouse Armani</h1>
+        <h1 class="h2"><?php echo isset($pageTitle) && !empty($pageTitle) ? $pageTitle : ""; ?></h1>
       </div>
-      <div class="col-md-5">
+      <!-- <div class="col-md-5">
         <ul class="breadcrumb d-flex justify-content-end">
           <li class="breadcrumb-item"><a href="index.html">Home</a></li>
           <li class="breadcrumb-item"><a href="shop-category.html">Ladies</a></li>
           <li class="breadcrumb-item"><a href="shop-category.html">Tops</a></li>
           <li class="breadcrumb-item active">White Blouse Armani</li>
         </ul>
-      </div>
+      </div> -->
     </div>
   </div>
 </div>
