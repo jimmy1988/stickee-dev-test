043ae435205e22a43b3325cc34779cc017d99c79
James McHugh <james.mchugh1988@gmail.com>
2019-10-29 20:06:58 +0000

    new array for the contact details and connection details

--- php/config.php
+++ php/config.php
@@ -3,4 +3,21 @@
   !defined("AUTHOR_CONTACT_NUMBER") ? define("AUTHOR_CONTACT_NUMBER", "07922955332") : "";
   !defined("AUTHOR_WEBSITE") ? define("AUTHOR_WEBSITE", "") : "";
   !defined("AUTHOR_EMAIL") ? define("AUTHOR_EMAIL", "james.mchugh1988@gmail.com") : "";
+  !defined("APP_NAME") ? define("APP_NAME", "Stickee Dev Test") : "";
+
+  $addressArray = array(
+    'Address 1' => "",
+    'Address 2' => "",
+    'City' => "",
+    'Postcode' => "",
+    'Country' => ""
+  );
+
+  !defined("CONTACT_INFO") ? define("CONTACT_INFO", $addressArray) : "";
+
+  !defined("DB_HOST") ? define("DB_HOST", "localhost") : "";
+  !defined("DB_USER") ? define("DB_USER", "stickeedevtestusr") : "";
+  !defined("DB_PASS") ? define("DB_PASS", "pqilcqtXRFb59JsB") : "";
+
+  $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS);
 ?>
