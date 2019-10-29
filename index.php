033257f07688fe3d446d766703a0b8e245f05831
James McHugh <james.mchugh1988@gmail.com>
2019-10-29 17:34:32 +0000

    new index page

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
-  include("../php/includes/top.php");
-  include("../php/includes/header.php");
+  include("php/config.php");
+  require_once("php/functions.php");
+  include("php/class/import_classes.php");
+  include("php/includes/top.php");
+  include("php/includes/topBar.php");
+  include("php/includes/header.php");
 ?>
 
 
 
 <?php
-  include("../php/includes/scripts.php");
-  include("../php/includes/footer.php");
-  include("../php/includes/bottom.php");
+  include("php/includes/scripts.php");
+  include("php/includes/footer.php");
+  include("php/includes/bottom.php");
 ?>
