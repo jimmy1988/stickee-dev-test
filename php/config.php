6448eba175f1f1cfcd6872754e0adbbf0bb3d557
James McHugh <james.mchugh1988@gmail.com>
2019-10-31 13:57:47 +0000

    added the total widget price

--- php/config.php
+++ php/config.php
@@ -1,9 +1,10 @@
 <?php
-  !defined("AUTHOR") ? define("AUTHOR", "James McHugh") : "";
-  !defined("AUTHOR_CONTACT_NUMBER") ? define("AUTHOR_CONTACT_NUMBER", "07922955332") : "";
-  !defined("AUTHOR_WEBSITE") ? define("AUTHOR_WEBSITE", "") : "";
-  !defined("AUTHOR_EMAIL") ? define("AUTHOR_EMAIL", "james.mchugh1988@gmail.com") : "";
-  !defined("APP_NAME") ? define("APP_NAME", "Stickee Dev Test") : "";
+  !defined("AUTHOR") ? define("AUTHOR", "James McHugh") : AUTHOR;
+  !defined("AUTHOR_CONTACT_NUMBER") ? define("AUTHOR_CONTACT_NUMBER", "07922955332") : AUTHOR_CONTACT_NUMBER;
+  !defined("AUTHOR_WEBSITE") ? define("AUTHOR_WEBSITE", "") : AUTHOR_WEBSITE;
+  !defined("AUTHOR_EMAIL") ? define("AUTHOR_EMAIL", "james.mchugh1988@gmail.com") : AUTHOR_EMAIL;
+  !defined("APP_NAME") ? define("APP_NAME", "Wally’s Widget Company (Stickee Dev Test)") : APP_NAME;
+  !defined("WIDGET_PRICE") ? define("WIDGET_PRICE", 0.10) : WIDGET_PRICE;
 
   $addressArray = array(
     'Address 1' => "32 Asbury Court",
@@ -13,10 +14,10 @@
     'Country' => "United Kingdom"
   );
 
-  !defined("CONTACT_INFO") ? define("CONTACT_INFO", $addressArray) : "";
+  !defined("CONTACT_INFO") ? define("CONTACT_INFO", $addressArray) : CONTACT_INFO;
 
-  !defined("DB_HOST") ? define("DB_HOST", "localhost") : "";
-  !defined("DB_USER") ? define("DB_USER", "stickeedevtestusr") : "";
-  !defined("DB_PASS") ? define("DB_PASS", "pqilcqtXRFb59JsB") : "";
-  !defined("DB_NAME") ? define("DB_NAME", "stickee-dev-test") : "";
+  !defined("DB_HOST") ? define("DB_HOST", "localhost") : DB_HOST;
+  !defined("DB_USER") ? define("DB_USER", "stickeedevtestusr") : DB_USER;
+  !defined("DB_PASS") ? define("DB_PASS", "pqilcqtXRFb59JsB") : DB_PASS;
+  !defined("DB_NAME") ? define("DB_NAME", "stickee-dev-test") : DB_NAME;
 ?>
