f64e4514691454101ed095021e30458cadf7d942
James McHugh <james.mchugh1988@gmail.com>
2019-11-01 11:22:48 +0000

    moved all ode out to external files

--- php/class/import_classes.php
+++ php/class/import_classes.php
@@ -1,5 +1,10 @@
 <?php
-  include("../config.php");
+  // echo "<pre>";
+  // print_r($_SERVER);
+  // echo "</pre>";
+  // die();
+
+  include($_SERVER['DOCUMENT_ROOT'] . "/php/config.php");
   include("class.DB.php");
   include("class.Product.php");
 
