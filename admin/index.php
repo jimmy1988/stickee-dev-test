c1f7667c5cb885f5b6875122e1e5dd29ad5070f6
James McHugh <james.mchugh1988@gmail.com>
2019-11-01 00:19:32 +0000

    basic template construct

---
+++ admin/index.php
@@ -0,0 +1,30 @@
+<?php
+
+  include("php/includes/top.php");
+  include("php/includes/header.php");
+  include("php/includes/sidebar.php");
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <?php
+    include("php/includes/titlebar.php");
+  ?>
+
+  <!-- Main content -->
+  <section class="content">
+
+    
+
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php
+
+  include("php/includes/footer.php");
+  include("php/includes/scripts.php");
+  include("php/includes/bottom.php");
+?>
